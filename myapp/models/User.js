8bade8364f806071db67c466c2b0f2ec8f6d5b2c
thejayadad <74875595+thejayadad@users.noreply.github.com>
2024-01-04 14:20:57 -0500

    update

---
+++ myapp/models/User.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    image: String,
+    emailVerified: Date,
+}, {timestamps: true})
+
+export default mongoose?.models?.User || mongoose.model("User", UserSchema)
---
+++ myapp/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#29d7cf',
+        secondary: '#8feae5'
+      }
+    },
+  },
+  plugins: [require("daisyui")],
+}
